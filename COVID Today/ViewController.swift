a900ea8594eb4c04b96660425835dfdba52fbb4d
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2021-01-15 06:54:55 -0800

    Initial Commit

---
+++ COVID Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID Today
+//
+//  Created by David Jr on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
